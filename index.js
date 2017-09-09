ce5b1afbdacf6eaedcadcf032939a746a040ceb9
Serge755 <loss2004@yandex.ru>
2017-09-10 01:24:18 +0300

    22222222

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+	console.log('client connected');
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
+	console.log(msg);
   });
 });
 
